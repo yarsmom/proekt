1c495e63f96810d37b2fe6865966102144c13792
Mazayd <garagulya699@gmail.com>
2024-05-29 21:45:21 +0300

    add cors

--- src/index.mjs
+++ src/index.mjs
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import morgan from 'morgan';
 import { userRouter } from './api/user/routers/user.router.mjs';
 import { feedRouter } from './api/feed/routes/feed.router.mjs';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ mongoose
 	.then((res) => console.log('Connected to DB'))
 	.catch((error) => console.log(error));
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan('common'));
 app.use('/api', [userRouter, feedRouter]);
